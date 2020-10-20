5448a09d609f67374810ab662012daa6f5cc22dd
deokgoo <kkddgg1001@gmail.com>
2020-10-20 17:42:37 +0900

    HabitTracker modify classComponent to functionalComponent

--- HabitTracker/src/components/habits.jsx
+++ HabitTracker/src/components/habits.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
-import Habit from "./habit";
+import React from 'react';
+import Habit from './habit';
 import './habits.css';
 
-class Habits extends Component {
-  render() {
-    const {
-      habits,
-      onInclement,
-      onDecrement,
-      onAdd,
-      onRemove,
-      onReset } = this.props;
-    return (
-      <div className="habits">
-        <ul>
-          {habits.map((habit) => (
-            <Habit
-              key={habit.id}
-              habit={habit}
-              onIncrement={onInclement}
-              onDecrement={onDecrement}
-              onAdd={onAdd}
-              onRemove={onRemove}
-            />
-          ))}
-        </ul>
-        <button className="btn-reset" onClick={onReset}>Reset All</button>
-      </div>
-    );
-  }
+const Habits = (props) => {
+  const {
+    habits,
+    onInclement,
+    onDecrement,
+    onAdd,
+    onRemove,
+    onReset } = props;
+  return (
+    <div className="habits">
+      <ul>
+        {habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={onInclement}
+            onDecrement={onDecrement}
+            onAdd={onAdd}
+            onRemove={onRemove}
+          />
+        ))}
+      </ul>
+      <button className="btn-reset" onClick={onReset}>Reset All</button>
+    </div>
+  );
 }
 
 export default Habits;
