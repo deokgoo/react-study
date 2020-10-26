d638037037667168d2d45d73050cba493b837825
deokgoo <kkddgg1001@gmail.com>
2020-10-26 23:51:28 +0900

    feature: set youtube api

--- youtube-clone/src/services/youtubeApi.js
+++ youtube-clone/src/services/youtubeApi.js
@@ -1,8 +1,25 @@
 // import axios from 'axios';
+const { REACT_APP_YOUTUBE_API_BASE_URL, REACT_APP_API_KEY }= process.env;
+
+const requestApi = (url, method) => {
+  const requestOptions = {
+    method,
+    redirect: 'follow',
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(url, requestOptions)
+      .then(res => resolve(res.json()))
+      .catch(err => reject(err));
+  })
+}
 
 export const getPopularVideos = async () => {
   console.log(process.env);
-  return process.env.YOUTUBE_API_BASE_URL;
+  const popularUrl = `${REACT_APP_YOUTUBE_API_BASE_URL}/videos?key=${REACT_APP_API_KEY}&part=snippet&chart=mostPopular&maxResults=25`;
+  const method = 'GET';
+  const responseData = await requestApi(popularUrl, method)
+  return responseData.items;
 }
 
 // export const getSearchVideo = async (query) => {
