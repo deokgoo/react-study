26e81119137a19197e7527a5bc1b75a8fcc2d1e3
deokgoo <kkddgg1001@gmail.com>
2020-11-03 22:59:45 +0900

    feature: complete popular video api

--- youtube-clone/src/components/video_card/video_card.jsx
+++ youtube-clone/src/components/video_card/video_card.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from "react";
+import style from './video_card.module.css';
 
 const VideoCard = (props) => {
   useEffect(() => {
     console.log(props);
   })
-  return <div>
-    <img src={props.video.snippet.thumbnails.high.url} alt=""/>
-    {props.video.snippet.title}
-  </div>;
+  return (
+    <div className={style.card}>
+      <img className={style.img} src={props.video.snippet.thumbnails.maxres.url} alt="video-title"/>
+      <div className={style.content}>
+        <div className="channel-logo"></div>
+        <div className={style.detail}>
+          <div className={style['title']}>{props.video.snippet.title}</div>
+          <div className={style['channel-name']}>{props.video.snippet.channelTitle}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
--- youtube-clone/src/components/video_list/video_list.jsx
+++ youtube-clone/src/components/video_list/video_list.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getPopularVideos } from '../../services/youtubeApi';
 import VideoCard from '../video_card/video_card';
+import style from './video_list.module.css';
 
 const VideoList = () => {
   const [videoList, setVideoList] = useState([]);
@@ -9,17 +10,18 @@ const VideoList = () => {
     const fetchData = async () => {
       try {
         const list = await getPopularVideos();
+        console.log(list);
         await setVideoList(list);
       } catch(err) {
         console.log('err', err);
-        setTimeout(() => fetchData(), 2000);
+        // setTimeout(() => fetchData(), 5000);
       }
     }
     fetchData();
   }, [])
 
   return (
-    <div className="video-list">
+    <div className={style.container}>
       {videoList ?
         videoList.map((x) =>
           <VideoCard video={x} key={x.id}/>
