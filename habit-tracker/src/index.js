73c3244bd58a6402daefd0a61ac4ecfa1d0d2036
deokgoo <kkddgg1001@gmail.com>
2021-06-09 23:23:58 +0900

    feature: habit tracker component

---
+++ habit-tracker/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/js/all';
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
