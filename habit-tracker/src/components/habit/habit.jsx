f7a4215045d90d4d827db1c6c4ae5adc81e1ed2c
deokgoo <kkddgg1001@gmail.com>
2021-06-10 22:17:49 +0900

    feature: insert state

--- habit-tracker/src/components/habit/habit.jsx
+++ habit-tracker/src/components/habit/habit.jsx
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import './habit.css';
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncrement = evt => {
+    this.setState({ count: this.state.count + 1 })
+  };
+  handleDecrement = evt => {
+    const cnt = this.state.count - 1;
+    this.setState({ count: cnt < 0 ? 0 : cnt });
+  }
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square">
           </i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-trash">
